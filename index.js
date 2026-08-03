264eb82248cb18a743c15a134ca4206d86d55950
Chiran Fernando <pcnfernando@gmail.com>
2026-08-03 20:07:33 +0530

    Add component-sub-type
    (cherry picked from commit fbfd76f057f76c8f6b347d65a8d45f6f910f38b0)

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ try {
     const gitHashDate = core.getInput('git-hash-date');
     const isAutoDeploy = core.getInput('is-auto-deploy') === 'true';
     const runId = core.getInput('run-id');
+    const organizationUuid = core.getInput('organizationUuid');
+    const componentSubType = core.getInput('component-sub-type');
 
     const choreoApp = process.env.CHOREO_GITOPS_REPO;
     let cluster_image_tags = [];
@@ -131,7 +133,8 @@ try {
         cluster_image_tags,
         git_hash_commit_timestamp: gitHashDate,
         is_auto_deploy: isAutoDeploy,
-        run_id: runId
+        run_id: runId,
+        component_sub_type: componentSubType
     };
 
     let WebhhookURL;
